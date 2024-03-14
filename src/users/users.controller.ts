9a6f054728ca172c4ed15e6dfecf91e086a2a910
DuyQuan <bddquan@gmail.com>
2024-03-14 14:36:40 +0700

    feat: NestJS controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    @Get() //GET /users or /users?role=value&age=18
+    findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+        return []
+    }
+
+    @Get('interns') //GET /users/interns
+    findAllInterns() {
+        return []
+    }
+
+    @Get(':id') //GET /users/:id
+    findOne(@Param('id') id: string) {
+        return { id }
+    }
+
+    @Post() //POST /users
+    create(@Body() user: {}) {
+        return user
+    }
+
+    @Patch(':id') //PATCH /users/:id
+    update(@Param('id') id: string, @Body() userUpdate: {}) {
+        return {id, ...userUpdate}
+    }
+
+    @Delete(':id') //DELETE /users/:id
+    delete(@Param('id') id: string) {
+        return id
+    }
+}
