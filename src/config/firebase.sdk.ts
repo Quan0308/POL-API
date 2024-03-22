cea4928f5ea4c09d128111ea83cf44d708df3be3
DuyQuan <bddquan@gmail.com>
2024-03-22 10:51:39 +0700

    POL-15 feat: testing connection with firebase

---
+++ src/config/firebase.sdk.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyDkt9rc6acZG-x5XjNXW_BWrW5ruPS32zk",
+  authDomain: "polary-8862a.firebaseapp.com",
+  projectId: "polary-8862a",
+  storageBucket: "polary-8862a.appspot.com",
+  messagingSenderId: "543502116747",
+  appId: "1:543502116747:web:3cd94a19a18e65ad27318e",
+  measurementId: "G-0FWCDSFP14"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
